605d75cf9b2d8f87b3f547f4abbe83d5dc5428c1
simonisaacs <simon.isaacs@oskfel.com>
2022-11-11 17:57:54 +0000

    new library

---
+++ object_store.py
@@ -0,0 +1,32 @@
+#region imports
+from AlgorithmImports import *
+
+''' <summary>
+ This is the class to handle the object store on QC
+'''
+from clr import AddReference
+AddReference("System")
+AddReference("QuantConnect.Algorithm")
+AddReference("QuantConnect.Common")
+AddReference("QuantConnect.Indicators")
+
+import enum
+import math
+from datetime import date, time, datetime, timedelta
+from System import *
+from copy import deepcopy
+from QuantConnect import *
+from QuantConnect import Market
+from QuantConnect.Algorithm import *
+from QuantConnect.Brokerages import *
+from QuantConnect.Orders import *
+from QuantConnect.Indicators import *
+from QuantConnect.Data import *
+from QuantConnect.Data.Market import *
+from QuantConnect.Data.Custom import *
+from QuantConnect.Data.Consolidators import *
+from QuantConnect.Python import *
+from structs_and_enums import *
+from dataclasses import dataclass
+from fusion_utils import *
+#endregion
